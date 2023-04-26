7fab276a1d0f291dac7a9ba0f6d6f0eeda8e2d31
RafaelSantosdaRosa <129983165+RafaelSantosdaRosa@users.noreply.github.com>
2023-04-26 16:31:11 -0300

    Add files via upload

---
+++ src/java/servlets/ValidaLogin.java
@@ -0,0 +1,69 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ValidaLogin", urlPatterns = {"/valida_login.java"})
+public class ValidaLogin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        
+        String userDb = "email@email.com";
+        String passDb = "senha1234";
+        
+        if(user.equals(userDb)&& pass.equals(passDb)){
+            request.setAttribute("userLogged", user);
+            request.getRequestDispatcher("home.jsp")
+                    .forward(request, response);
+        }else{
+           PrintWriter out = response.getWriter();
+           out.print(
+                "<script>"
+                +"alert('Acesso negado')"
+                +"window.location.replace('index.html');"
+                +"</script>"
+           );
+        }
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ValidaLogin</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ValidaLogin at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
